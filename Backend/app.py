65f9e113f160af90114202aabc84749c2ce500d2
unbxd <unbxd@Dheeraj-Reddy.local>
2023-08-07 14:57:59 +0530

    fix(docker-compose): connect to all containers

--- Backend/app.py
+++ Backend/app.py
@@ -1,24 +1,25 @@
 import os
+import logging
 from flask import Flask
 from models import db
 import redis
-import socket
-
+    
 def create_app():
     app = Flask(__name__)
-
+    app.logger.setLevel(logging.INFO)
     # Modify the database URI to use the service name as the host in the connection URL
-    #db_host = os.environ.get('DATABASE_HOST', 'localhost')
-    #print(f"Database Host: {db_host}") 
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://unbxd:unbxd@127.0.0.1/unbxddatabase'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://unbxd:unbxd@database-container:5432/unbxddatabase'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
 
     # Initialize the SQLAlchemy db object with the app
     db.init_app(app)
 
     # Modify the Redis URL to use the service name as the host in the connection URL
-    redis_url = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+    redis_url = os.environ.get('REDIS_URL', 'redis://redis-container:6379')
     app.redis_client = redis.StrictRedis.from_url(redis_url)
+
+    # Set the logging level to INFO
 
 
     # Import blueprints here to avoid circular imports
@@ -36,11 +37,15 @@ def create_app():
     # Function to create tables before each request
     @app.before_request
     def create_tables():
+        print("Creating database tables...")
         db.create_all()
+        print("Database tables created!")
 
     return app
 
 # If this file is being executed as the main module, run the app
 if __name__ == '__main__':
     app = create_app()
+
+    # Run the app in debug mode
     app.run(debug=True, host="0.0.0.0", port=5000)
--- Backend/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start Redis server in the background
-redis-server &
-
-# Start the Flask application
-python3 app.py
